5541653de7cabb8365ae376a52255388713a1f10
BT_1N3_29 <vli8558@bths.edu>
2018-11-21 09:40:36 -0500

    done STRING compare

--- src/Runner.java
+++ src/Runner.java
@@ -6,14 +6,22 @@ public class Runner {
         {
             test[i] = (int)(Math.random()*1001);
         }
+          long time = System.nanoTime();
         mergeSort(test);
+        System.out.println((double)(System.nanoTime() - time)/(100000000));
 
-        for(int i=0;i<test.length;i++)
-        {
-            System.out.println(test[i]);
+        for(int i=0;i<test.length;i++) {
+            System.out.print(test[i] + " ");
         }
-
-
+        String[] test2 = new String[10];
+        for(int i=0;i<test2.length;i++)
+        {
+            test2 = randomStringArr(test2.length,10);
+        }
+        mergeSort(test2);
+        for(int i=0;i<test2.length;i++) {
+            System.out.print(test2[i] + " ");
+        }
     }
     public static void mergeSort(int[] arr)
     {
@@ -66,4 +74,74 @@ public class Runner {
         }
 
     }
+    public static void mergeSort(String[] arr)
+    {
+        int n= arr.length;
+        String[] temp = new String[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
+    public static void mergeSortHelper(String[] arr,int left,int right, String[] temp)
+    {
+        if(left<right)
+        {
+            int mid = (left+right)/2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr,mid+1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+    public static void merge(String[] arr,int left,int mid,int right,String[] temp)
+    {
+        int i=left;
+        int j=mid+1;
+        int k=left;
+        while(i<=mid && j<=right){
+            if(arr[i].compareTo(arr[j])<0)
+            {
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+
+        }
+        while(i<=mid){
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j<=right)
+        {
+            temp[k]  =arr[j] ;
+            j++;
+            k++;
+        }
+        for(int z=left;z<=right;z++)
+        {
+            arr[z] = temp[z];
+        }
+
+    }
+    public static String[] randomStringArr(int num, int length){
+        String[] arr = new String[num];
+        while(num>0)
+        {
+            int i=0;
+            String s = "";
+            while(i<length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s+c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
+
+    }
+
 }
